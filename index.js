f0d77e41f676575cff3b16f1c89f28351b07399b
Gianluca Di Bella <gianluca.dib99@gmail.com>
2022-09-26 10:26:51 -0400

    change on verify route

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/verify', async (req, res) => {
-    console.log('test production')
+
     if(!req.body.token) {
         return res.status(400).json({error: 'No token provided'});
     }
